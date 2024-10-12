60ad1f59c54a293d7cda3eeac4b95a7a0e178fcc
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-12 23:16:01 +0500

    feat: implement os functions

--- src/index.js
+++ src/index.js
@@ -44,16 +44,69 @@ let currentDirectory = process.cwd();
 console.log(`You are currently in ${currentDirectory}\n`);
 
 const question = () => {
-    rl.question('Please enter your command: \n', (command) => {
-        if (command === 'up') {
-            const parentDirectory = path.dirname(currentDirectory);
-            process.chdir(parentDirectory);
-            currentDirectory = process.cwd();
-            console.log(`\nYou are currently in ${currentDirectory}\n`);
-            question();
-        }
-        if (command === '.exit') {
-            process.exit();
+    rl.question('Please enter your command: \n', async (command) => {
+        switch (command) {
+            case 'up':
+                const parentDirectory = path.dirname(currentDirectory);
+                process.chdir(parentDirectory);
+                currentDirectory = process.cwd();
+                console.log(`\nYou are currently in ${currentDirectory}\n`);
+                question();
+                break;
+            case 'ls':
+                try {
+                    const directoryContent = await readdir(currentDirectory, { withFileTypes: true});
+    
+                    const sortedDirectoryContent = await directoryContent.toSorted((a, b) => {
+                        if (a.isDirectory() && !b.isDirectory()) 
+                            return -1;
+                        if (!a.isDirectory() && b.isDirectory())
+                            return 1;
+                    })
+    
+                    console.table(sortedDirectoryContent.map(content => ({
+                        Name: content.name,
+                        Type: content.isDirectory() ? 'Directory' : 'File'
+                    })))
+    
+                    question();
+                } catch {
+                    console.error('Operation failed');
+                }
+                break;
+            case 'os --EOL': 
+                const EOL = os.EOL;
+                console.log(`EOL: ${JSON.stringify(EOL)}`);
+                question();
+                break;
+            case 'os --cpus':
+                const cpus = os.cpus();
+                console.log(`CPUs info:\nCPUs: ${cpus.length}`);
+                console.table(cpus.map(cpu => ({
+                    Model: cpu.model,
+                    Speed: cpu.speed/1000+' GHz'
+                })))
+                question();
+                break;
+            case 'os --homedir':
+                const homeDir = os.homedir();
+                console.log(`Home directory: ${homeDir}`)
+                question();
+            case 'os --username':
+                const userName = os.userInfo();
+                console.log(`Username: ${userName.username}`);
+                question();
+                break;
+            case 'os --architecture':
+                const CPUArchitecture = os.arch();
+                console.log(`CPU architecture: ${CPUArchitecture}`);
+                question();
+                break;
+            case '.exit':
+                process.exit();
+            default:
+                console.log('Invalid input \n');
+                question();
         }
     });
 }
